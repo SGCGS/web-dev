6e49ea628d43074200a92faedc405263b11822ff
UFervor <micsof365@gmail.com>
2023-05-21 15:40:08 +0800

    May.21.2023.f

--- src/components/LoginPage.vue
+++ src/components/LoginPage.vue
@@ -3,10 +3,16 @@
         <h2>Login with <span style="color: #0072FF;">ManageBac</span></h2>
         <form @submit="handleSubmit" ref="loginForm">
             <label style="font-weight: bold;">Username</label>
-            <input class="form-input" name="username" v-model="username">
+            <input class="form-input" name="username" v-model="username" required>
             <label style="font-weight: bold;">Password</label>
-            <input class="form-input" type="password" name="password" v-model="password">
-            <button type="submit" class="form-button">Login</button>
+            <input class="form-input" type="password" name="password" v-model="password" required>
+            <button type="submit" :disabled="isLoading || isError" class="form-button">
+                <span v-if="isLoading">Loading...</span>
+                <template v-else-if="isError">
+                    {{ errorDetail }} ({{ remainingSeconds }})
+                </template>
+                <span v-else>Login</span>
+            </button>
         </form>
     </div>
     <ColorBackground :background="background" />
@@ -18,26 +24,57 @@ import ColorBackground from './ColorBackgroundG2.vue';
 import { useReCaptcha } from 'vue-recaptcha-v3';
 import Cookies from 'js-cookie';
 import { useRouter } from 'vue-router';
+import { ref } from 'vue';
 
 export default {
     setup() {
         const router = useRouter();
+
         function validateAuthorization() {
             return Cookies.get("token");
         }
+
         if (validateAuthorization()) {
             router.push("/");
         }
-        const { executeRecaptcha, recaptchaLoaded } = useReCaptcha()
+
+        const { executeRecaptcha, recaptchaLoaded } = useReCaptcha();
         const recaptcha = async () => {
-            await recaptchaLoaded()
-            const token = await executeRecaptcha('login')
-            console.log({ token })
+            await recaptchaLoaded();
+            const token = await executeRecaptcha('login');
             return token;
-        }
+        };
+
+        const isLoading = ref(false);
+        const isError = ref(false);
+        const errorDetail = ref('');
+        const remainingSeconds = ref(0);
+        let countdownTimer = null;
+
+        const startCountdown = () => {
+            let seconds = 3;
+            remainingSeconds.value = seconds;
+
+            countdownTimer = setInterval(() => {
+                seconds--;
+                remainingSeconds.value = seconds;
+
+                if (seconds === 0) {
+                    clearInterval(countdownTimer);
+                    isError.value = false;
+                    errorDetail.value = '';
+                }
+            }, 1000);
+        };
+
         return {
-            recaptcha
-        }
+            recaptcha,
+            isLoading,
+            isError,
+            errorDetail,
+            remainingSeconds,
+            startCountdown
+        };
     },
     name: 'LoginPage',
     components: {
@@ -54,39 +91,49 @@ export default {
     methods: {
         async handleSubmit(event) {
             event.preventDefault();
-            this.rt = await this.recaptcha();
+            const form = this.$refs.loginForm;
+            if (form.checkValidity()) {
+                this.isLoading = true;
 
-            const payload = {
-                username: this.username,
-                password: this.password,
-                rt: this.rt
-            };
+                this.rt = await this.recaptcha();
 
-            const response = await fetch(`${this.$apiBaseUrl}/login/managebac`, {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json'
-                },
-                body: JSON.stringify(payload)
-            });
+                const payload = {
+                    username: this.username,
+                    password: this.password,
+                    rt: this.rt
+                };
 
-            if (response.ok) {
-                const cookie = response.json()["token"];
-                var now = new Date();
-                var future = new Date(now.getTime() + this.$expireInterval * 60000);
-                Cookies.set('token', cookie, { expires: future });
-                const urlParams = new URLSearchParams(window.location.search);
-                const redirectParam = urlParams.get('redirect');
-                if (redirectParam) {
-                    this.$router.push(redirectParam);
-                } else {
-                    this.$router.push('/');
+                const response = await fetch(`${this.$apiBaseUrl}/login/managebac`, {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json'
+                    },
+                    body: JSON.stringify(payload)
+                });
+
+                if (response.ok) {
+                    const responseData = await response.json();
+                    const cookie = responseData.token;
+                    var now = new Date();
+                    var future = new Date(now.getTime() + this.$expireInterval * 60000);
+                    Cookies.set('token', cookie, { expires: future });
+                    const urlParams = new URLSearchParams(window.location.search);
+                    const redirectParam = urlParams.get('redirect');
+                    if (redirectParam) {
+                        this.$router.push(redirectParam);
+                    } else {
+                        this.$router.push('/');
+                    }
+                } else if (response.status === 401) {
+                    const errorJson = await response.json();
+                    this.isError = true;
+                    this.errorDetail = errorJson.detail;
+                    this.startCountdown();
                 }
-            } else if (response.status === 401) {
-                const errorJson = await response.json();
-                alert(errorJson["detail"]);
+
+                this.isLoading = false;
             }
-        },
+        }
     }
 };
 </script>
